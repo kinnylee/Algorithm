4c1fc09f9aa1ae41fd757545e3b972444ea56838
kinnylee <853251563@qq.com>
2015-11-25 20:55:51 +0800

    commit

--- src/Sort/main.cpp
+++ src/Sort/main.cpp
@@ -1,10 +1,16 @@
 #include <process.h>
 
 #include "SortTest.h"
+#include "BinaryArray.h"
+
+unsigned a[10] = { 1, 0, 0, 1, 0, 1, 0, 0, 1, 1 };
+unsigned b[10] = { 1, 0, 0, 1, 1, 0, 0, 1, 1, 1 };
 
 int main()
 {
     SortTest::InsertSortTest();
 
+    //unsigned *c = new unsigned[11];
+    //BinaryArray::Add(a, b, 10, c);
     system("pause");
 }
